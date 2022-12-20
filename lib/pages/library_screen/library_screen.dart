60bd799920af8241c7ba10ab355d0b671193caa2
Rob Vandelinder <robjvan@gmail.com>
2022-12-20 14:41:01 -0330

    Added temp logout button

--- lib/pages/library_screen/library_screen.dart
+++ lib/pages/library_screen/library_screen.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:get/get.dart';
 // import 'package:librarian/actions/user_settings_actions.dart';
 // import 'package:librarian/screens/library_screen/library_screen_view_model.dart';
 // import 'package:librarian/state.dart';
@@ -7,6 +9,7 @@ import 'package:flutter_redux/flutter_redux.dart';
 // import 'package:librarian/widgets/widgets.dart';
 import 'package:librarian_frontend/actions/actions.dart';
 import 'package:librarian_frontend/pages/library_screen/library_screen_view_model.dart';
+import 'package:librarian_frontend/pages/pages.dart';
 import 'package:librarian_frontend/state.dart';
 import 'package:librarian_frontend/widgets/widgets.dart';
 import 'package:redux/redux.dart';
@@ -34,8 +37,21 @@ class LibraryScreen extends StatelessWidget {
                   ? const Scaffold(
                       body: Center(child: CircularProgressIndicator()),
                     )
-                  : const Scaffold(
-                      body: Center(child: Text('Library Screen')),
+                  : Scaffold(
+                      body: SafeArea(
+                        child: Column(
+                          children: [
+                            Text('Library Screen'),
+                            ElevatedButton(
+                              onPressed: () {
+                                FirebaseAuth.instance.signOut();
+                                Get.offAll(new LoginScreen());
+                              },
+                              child: Text('Logout'),
+                            ),
+                          ],
+                        ),
+                      ),
                       // drawer: SettingsDrawer(
                       //   user: FirebaseAuth.instance.currentUser,
                       // ),
