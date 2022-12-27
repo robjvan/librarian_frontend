d2ad514844aa0cba76863164cf387b0d9ee1e04f
Rob Vandelinder <robjvan@gmail.com>
2022-12-27 08:31:42 -0330

    Added ForgotPass screen

--- lib/pages/forgot_password_screen/forgot_password_screen.dart
+++ lib/pages/forgot_password_screen/forgot_password_screen.dart
@@ -1,23 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:get/get.dart';
+import 'package:librarian_frontend/pages/forgot_password_screen/forgot_password_screen_view_model.dart';
+import 'package:librarian_frontend/state.dart';
+import 'package:librarian_frontend/utilities/authentication.dart';
 
-// TODO(Rob): Add 'Forgot password' functionality
-
-class ForgotPasswordScreen extends StatelessWidget {
+class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({final Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final TextEditingController emailController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String userEmail = '';
+
+  Widget _buildSubheader(final ForgotPasswordScreenViewModel vm) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Text('Forgot password', style: vm.subheaderStyle),
+    );
+  }
+
+  Widget _buildHeader(final double sh, final ForgotPasswordScreenViewModel vm) {
+    return SizedBox(
+      height: sh / 3,
+      child: Hero(
+        tag: 'headerHero',
+        child: Material(
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Image.asset('assets/images/bookshelf.jpg', fit: BoxFit.cover),
+              Center(
+                child: Text(
+                  'app-title'.tr,
+                  textAlign: TextAlign.center,
+                  style: vm.titleStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEmailField() {
+    return TextFormField(
+      onChanged: (final String newVal) => userEmail = newVal,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      controller: emailController,
+      decoration: InputDecoration(
+        labelText: 'login.email'.tr,
+        labelStyle: const TextStyle(fontSize: 20),
+      ),
+      validator: (final String? text) {
+        if (text == null || text.isEmpty) {
+          // return 'Email cannot be empty!';
+          return 'login.empty-email'.tr;
+        } else if (!RegExp(
+          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+        ).hasMatch(text)) {
+          // return 'Email must be proper format!';
+          return 'login.email-format'.tr;
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (_formKey.currentState!.validate()) {
+          AuthService.sendPasswordResetEmail(emailController.text);
+          Get.snackbar(
+            '',
+            'login.verify-email'.tr,
+            snackPosition: SnackPosition.TOP,
+          );
+
+          Get.back();
+        }
+      },
+      child: const Text('Go Back'),
+    );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[_buildEmailField(), _buildSubmitButton()],
+      ),
+    );
+  }
 
   @override
   // ignore: prefer_expression_function_bodies
   Widget build(final BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          const Text('Forgot password'),
-          ElevatedButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text('Go Back'),
+    return StoreConnector<GlobalAppState, ForgotPasswordScreenViewModel>(
+      distinct: true,
+      converter: ForgotPasswordScreenViewModel.create,
+      builder: (
+        final BuildContext context,
+        final ForgotPasswordScreenViewModel vm,
+      ) {
+        final double sh = MediaQuery.of(context).size.height;
+
+        return Scaffold(
+          body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                _buildHeader(sh, vm),
+                _buildSubheader(vm),
+                _buildForm(),
+              ],
+            ),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/pages/forgot_password_screen/forgot_password_screen_view_model.dart
+++ lib/pages/forgot_password_screen/forgot_password_screen_view_model.dart
@@ -1,12 +1,33 @@
+import 'package:flutter/material.dart';
 import 'package:librarian_frontend/state.dart';
 import 'package:redux/redux.dart';
 
+@immutable
 class ForgotPasswordScreenViewModel {
-  ForgotPasswordScreenViewModel();
+  final TextStyle titleStyle;
+  final TextStyle subheaderStyle;
+
+  const ForgotPasswordScreenViewModel({
+    required this.titleStyle,
+    required this.subheaderStyle,
+  });
 
   factory ForgotPasswordScreenViewModel.create(
     final Store<GlobalAppState> store,
   ) {
-    return ForgotPasswordScreenViewModel();
+    return const ForgotPasswordScreenViewModel(
+      titleStyle: TextStyle(
+        fontFamily: 'CarroisSC',
+        fontSize: 64,
+        fontWeight: FontWeight.w600,
+        color: Color(0xFFFFFFFF),
+      ),
+      subheaderStyle: TextStyle(
+        fontFamily: 'CarroisSC',
+        fontSize: 32,
+        fontWeight: FontWeight.w600,
+        color: Color(0xFF424242),
+      ),
+    );
   }
 }
