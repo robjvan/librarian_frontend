af72eb2fd4bc115a57d36834ac0502b23ccfbb9c
Rob Vandelinder <robjvan@gmail.com>
2023-01-05 13:59:43 -0330

    Changed context to final

--- lib/pages/new_book_screen/widgets/image_grabber_dialog.dart
+++ lib/pages/new_book_screen/widgets/image_grabber_dialog.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class ImageGrabberDialog extends StatefulWidget {
   const ImageGrabberDialog({super.key});
@@ -10,7 +9,7 @@ class ImageGrabberDialog extends StatefulWidget {
 
 class _ImageGrabberDialogState extends State<ImageGrabberDialog> {
   @override
-  Widget build(BuildContext context) {
+  Widget build(final BuildContext context) {
     return Container();
     // TODO(Rob): Build image grabber dialog
   }
