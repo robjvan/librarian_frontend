6c0acaf804e5f867943638f67601f09d357b7667
Rob Vandelinder <robjvan@gmail.com>
2023-01-05 13:36:04 -0330

    Added TextStyles

--- lib/pages/book_details_screen/book_details_screen_view_model.dart
+++ lib/pages/book_details_screen/book_details_screen_view_model.dart
@@ -8,26 +8,42 @@ class BookDetailsScreenViewModel {
   final Color canvasColor;
   final Color userColor;
   final Function(dynamic) dispatch;
+  final TextStyle unavailableStyle;
+  final TextStyle pageCountStyle;
 
   BookDetailsScreenViewModel({
     required this.textColor,
     required this.canvasColor,
     required this.userColor,
     required this.dispatch,
+    required this.unavailableStyle,
+    required this.pageCountStyle,
   });
 
   factory BookDetailsScreenViewModel.create(final Store<GlobalAppState> store) {
     bool _checkDarkMode() => store.state.userSettings.useDarkMode;
 
+    Color getTextColor() {
+      return _checkDarkMode()
+          ? AppColors.darkModeTextColor
+          : AppColors.lightModeTextColor;
+    }
     return BookDetailsScreenViewModel(
       canvasColor: _checkDarkMode()
           ? AppColors.darkModeBgColor
           : AppColors.lightModeBgColor,
-      textColor: _checkDarkMode()
-          ? AppColors.darkModeTextColor
-          : AppColors.lightModeTextColor,
+      textColor: getTextColor(),
       userColor: store.state.userSettings.userColor,
       dispatch: store.dispatch,
+      unavailableStyle: TextStyle(
+        color: getTextColor().withOpacity(0.6),
+        fontStyle: FontStyle.italic,
+      ),
+      pageCountStyle: TextStyle(
+        fontStyle: FontStyle.italic,
+        color: getTextColor(),
+        fontSize: 16,
+      ),
     );
   }
 }
