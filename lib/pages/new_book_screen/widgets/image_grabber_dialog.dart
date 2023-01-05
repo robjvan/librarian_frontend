0e3245966b868f06d2700e6dfc7f3fca8997ea03
Rob Vandelinder <robjvan@gmail.com>
2023-01-04 23:10:32 -0330

    Added image grabber dialog

---
+++ lib/pages/new_book_screen/widgets/image_grabber_dialog.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ImageGrabberDialog extends StatefulWidget {
+  const ImageGrabberDialog({super.key});
+
+  @override
+  State<ImageGrabberDialog> createState() => _ImageGrabberDialogState();
+}
+
+class _ImageGrabberDialogState extends State<ImageGrabberDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+    // TODO(Rob): Build image grabber dialog
+  }
+}
