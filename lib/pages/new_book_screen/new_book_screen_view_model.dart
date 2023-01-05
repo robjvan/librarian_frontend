3b9c24bb3778dd796fc5266c733b927cb090d342
Rob Vandelinder <robjvan@gmail.com>
2023-01-05 13:37:38 -0330

    Added submitFn

--- lib/pages/new_book_screen/new_book_screen_view_model.dart
+++ lib/pages/new_book_screen/new_book_screen_view_model.dart
@@ -5,6 +5,7 @@ import 'package:librarian_frontend/models/models.dart';
 import 'package:librarian_frontend/state.dart';
 import 'package:librarian_frontend/utilities/theme.dart';
 import 'package:redux/redux.dart';
+import 'package:uuid/uuid.dart';
 
 @immutable
 class NewBookScreenViewModel {
@@ -15,7 +16,21 @@ class NewBookScreenViewModel {
   final TextStyle titleStyle;
   final String? Function(String? s)? titleValidatorFn;
   final String? Function(String? s)? numberValidatorFn;
-  final Function(dynamic) submitFn;
+  final Function({
+    required String title,
+    String? author,
+    String? description,
+    String? publisher,
+    int? isbn,
+    int? publishYear,
+    int? pageCount,
+    double? rating,
+    bool? addToFaves,
+    bool? addToShoppingList,
+    bool? addToWishlist,
+    bool? alreadyRead,
+    String? thumbnailUrl,
+  }) submitFn;
 
   NewBookScreenViewModel({
     required this.textColor,
@@ -35,13 +50,72 @@ class NewBookScreenViewModel {
         ? AppColors.darkModeTextColor
         : AppColors.lightModeTextColor;
 
-    dynamic _submitFn(dynamic) {
-      return () {
-        // if (formKey.currentState!.validate()) {
-        //   final Book newBook = Book(authors: []);
-        //   vm.dispatch(AddBookToCollectionAction(newBook));
-        // }
-      };
+    dynamic _submitFn({
+      required final String title,
+      final String? author,
+      final String? description,
+      final String? publisher,
+      final int? isbn,
+      final int? publishYear,
+      final int? pageCount,
+      final double? rating,
+      final bool? addToFaves,
+      final bool? addToShoppingList,
+      final bool? addToWishlist,
+      final bool? alreadyRead,
+      String? thumbnailUrl,
+    }) {
+      const Uuid uuid = Uuid();
+      final String _newId = uuid.v4();
+
+      final String sortTitle = title.startsWith(RegExp('The '))
+          //Title start with 'The'
+          ? title.replaceFirst(RegExp('The '), '')
+          // No 'The' in title,
+          : title.startsWith(RegExp('A '))
+              // Title starts with 'A'
+              ? title.replaceFirst(RegExp('A '), '')
+              // No 'The' or 'A' in title
+              : title.startsWith(RegExp('An '))
+                  // Title start with 'An'
+                  ? title.replaceFirst(RegExp('An '), '')
+                  // Title does not start with 'The', 'A', or 'An'
+                  : title;
+
+      String sortAuthor = '';
+      if (author != null) {
+        sortAuthor = author.split(' ').length > 1
+            ? author.split(' ').length > 2
+                ? '${author.split(" ")[2]}, ${author.split(" ")[0]} ${author.split(" ")[1]}'
+                : '${author.split(" ")[1]}, ${author.split(" ")[0]}'
+            : author;
+      }
+
+      final Book newBook = Book(
+        title: title,
+        sortTitle: sortTitle,
+        authors: <String>[author ?? ''],
+        sortAuthor: sortAuthor,
+        description: description ?? '',
+        haveRead: alreadyRead ?? false,
+        inFavesList: addToFaves ?? false,
+        inShoppingList: addToShoppingList ?? false,
+        inWishList: addToWishlist ?? false,
+        isMature: false,
+        isbn10: isbn.toString().length == 10 ? isbn.toString() : '',
+        isbn13: isbn.toString().length == 13 ? isbn.toString() : '',
+        pageCount: pageCount,
+        publishYear: publishYear.toString(),
+        publisher: publisher ?? '',
+        rating: rating ?? 0,
+        readCount: 0,
+        thumbnail: thumbnailUrl != ''
+            ? thumbnailUrl
+            : 'http://placekitten.com/200/300',
+        id: _newId,
+      );
+
+      store.dispatch(AddBookToCollectionAction(newBook));
     }
 
     return NewBookScreenViewModel(
@@ -64,9 +138,12 @@ class NewBookScreenViewModel {
       },
       numberValidatorFn: (final String? s) {
         // TODO: Fix RegExp validators
-        if (!RegExp('[!a-zA-Z]').hasMatch(s!)) {
+        if (s!.contains(' ') && s.contains('-') && s.contains('.')) {
           return 'Numbers only!';
         }
+        // if (s!.contains(RegExp('[0-9]'))) {
+        //   return 'Numbers only!';
+        // }
       },
       submitFn: _submitFn,
     );
