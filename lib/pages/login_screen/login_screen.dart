20e36994d818f5b7628c97b11e6c8dbb9a1b294f
Rob Vandelinder <robjvan@gmail.com>
2023-03-06 22:41:29 -0330

    Converted inline expressions to regular

--- lib/pages/login_screen/login_screen.dart
+++ lib/pages/login_screen/login_screen.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:get/get.dart';
 import 'package:librarian_frontend/pages/login_screen/login_screen_view_model.dart';
-import 'package:librarian_frontend/pages/login_screen/widgets/login_header/login_header.dart';
-import 'package:librarian_frontend/pages/login_screen/widgets/widgets.dart';
 import 'package:librarian_frontend/pages/pages.dart';
-import 'package:librarian_frontend/state.dart';
 import 'package:librarian_frontend/services/authentication.dart';
+import 'package:librarian_frontend/state.dart';
+import 'package:librarian_frontend/utilities/theme.dart';
+import 'package:librarian_frontend/widgets/widgets.dart';
 import 'package:redux/redux.dart';
 
 final CollectionReference<Map<String, dynamic>> usersRef =
@@ -45,56 +45,60 @@ class _LoginScreenState extends State<LoginScreen> {
     super.initState();
   }
 
-  Widget _buildEmailField() => TextFormField(
-        onChanged: (final String newVal) => userEmail = newVal,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        controller: emailController,
-        decoration: InputDecoration(
-          labelText: 'login.email'.tr,
-          labelStyle: const TextStyle(fontSize: 20),
-        ),
-        validator: (final String? text) {
-          if (text == null || text.isEmpty) {
-            // return 'Email cannot be empty!';
-            return 'login.empty-email'.tr;
-          } else if (!RegExp(
-            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
-          ).hasMatch(text)) {
-            // return 'Email must be proper format!';
-            return 'login.email-format'.tr;
-          }
-          return null;
-        },
-      );
+  Widget _buildEmailField() {
+    return TextFormField(
+      onChanged: (final String newVal) => userEmail = newVal,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      controller: emailController,
+      decoration: InputDecoration(
+        labelText: 'login.email'.tr,
+        labelStyle: const TextStyle(fontSize: 20),
+      ),
+      validator: (final String? text) {
+        if (text == null || text.isEmpty) {
+          // return 'Email cannot be empty!';
+          return 'login.empty-email'.tr;
+        } else if (!RegExp(
+          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+        ).hasMatch(text)) {
+          // return 'Email must be proper format!';
+          return 'login.email-format'.tr;
+        }
+        return null;
+      },
+    );
+  }
 
-  Widget _buildPasswordField() => TextFormField(
-        obscureText: _passwordObscured,
-        onChanged: (final String newPass) => userPass = newPass,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        controller: passwordController,
-        decoration: InputDecoration(
-          labelText: 'login.password'.tr,
-          labelStyle: const TextStyle(fontSize: 20),
-          suffixIcon: IconButton(
-            icon: Icon(
-              _passwordObscured
-                  ? Icons.visibility_outlined
-                  : Icons.visibility_off_outlined,
-            ),
-            onPressed: () {
-              setState(() {
-                _passwordObscured = !_passwordObscured;
-              });
-            },
+  Widget _buildPasswordField() {
+    return TextFormField(
+      obscureText: _passwordObscured,
+      onChanged: (final String newPass) => userPass = newPass,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      controller: passwordController,
+      decoration: InputDecoration(
+        labelText: 'login.password'.tr,
+        labelStyle: const TextStyle(fontSize: 20),
+        suffixIcon: IconButton(
+          icon: Icon(
+            _passwordObscured
+                ? Icons.visibility_outlined
+                : Icons.visibility_off_outlined,
           ),
+          onPressed: () {
+            setState(() {
+              _passwordObscured = !_passwordObscured;
+            });
+          },
         ),
-        validator: (final String? text) {
-          if (text == null || text.isEmpty) {
-            return 'login.empty-password'.tr;
-          }
-          return null;
-        },
-      );
+      ),
+      validator: (final String? text) {
+        if (text == null || text.isEmpty) {
+          return 'login.empty-password'.tr;
+        }
+        return null;
+      },
+    );
+  }
 
   Widget _buildGoogleButton() {
     return const Padding(
@@ -103,49 +107,56 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget _buildEmailSignInButton(final LoginScreenViewModel vm) => Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32),
-        child: OutlinedButton(
-          onPressed: () async {
-            if (_formKey.currentState!.validate()) {
-              vm.signInWithEmail(
-                context,
-                emailController.text,
-                passwordController.text,
-              );
-            }
-          },
-          style: vm.emailSigninButtonStyle,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
-                child: Text(
-                  'login.sign-in'.tr,
-                  style: const TextStyle(
-                    color: Color(0xFF606060),
-                    fontSize: 20,
-                  ),
+  Widget _buildEmailSignInButton(final LoginScreenViewModel vm) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: OutlinedButton(
+        onPressed: () async {
+          if (_formKey.currentState!.validate()) {
+            vm.signInWithEmail(
+              context,
+              emailController.text,
+              passwordController.text,
+            );
+          }
+        },
+        style: vm.emailSigninButtonStyle,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+              child: Text(
+                'sign-in'.tr,
+                style: const TextStyle(
+                  color: Color(0xFF606060),
+                  fontSize: 20,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 
-  Widget _buildCreateAcctButton(final LoginScreenViewModel vm) => TextButton(
-        child: Text(
-          'login.create-account'.tr,
-          style: vm.createAccountButtonStyle,
-        ),
-        onPressed: () => Get.to(() => const SignUpScreen()),
-      );
+  Widget _buildCreateAcctButton(final LoginScreenViewModel vm) {
+    return TextButton(
+      child: Text(
+        'login.create-account'.tr,
+        style: AppTextStyles.createAccountButtonStyle,
+      ),
+      onPressed: () => Get.to(() => const SignUpScreen()),
+    );
+  }
 
-  Widget _buildForgotPassButton(final LoginScreenViewModel vm) => TextButton(
-        child: Text('login.forgot-password'.tr, style: vm.passwordButtonStyle),
-        onPressed: () => Get.to(() => const ForgotPasswordScreen()),
-      );
+  Widget _buildForgotPassButton(final LoginScreenViewModel vm) {
+    return TextButton(
+      child: Text('login.forgot-password'.tr,
+          style: AppTextStyles.passwordButtonStyle),
+      onPressed: () => Get.to(() => const ForgotPasswordScreen()),
+    );
+  }
 
   Widget _buildForm() {
     return Padding(
